a27064aaebd5c779c5fb8841d27704b18ae559fc
sq <shane.quinn197@gmail.com>
2022-04-24 18:43:15 +0100

    Split data gens

--- src/data.py
+++ src/data.py
@@ -9,6 +9,7 @@ import os
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import logging
 import math
+import shutil
 
 
 
@@ -17,7 +18,7 @@ logger = logging.getLogger(__name__)
 
 
 
-def get_fer_2013_data_gens(data_path = r'../data', batch_size = 64, img_size = (64, 64)):
+def get_pre_split_data_gens(data_path = r'../data', batch_size = 64, img_size = (64, 64)):
     """
     Ensure FER-2013 dataset is stored in data subdirectory, if not modify train_dir and val_dir paths
     
@@ -34,8 +35,7 @@ def get_fer_2013_data_gens(data_path = r'../data', batch_size = 64, img_size = (
     y_test_gen : IMAGE DATA GENERATOR 
         Test data generator.
     """
-    
-
+    # logger = logging.getLogger(__name__) 
     train_dir = data_path + r'/train'
     val_dir = data_path + r'/test'
     
@@ -68,7 +68,7 @@ def get_fer_2013_data_gens(data_path = r'../data', batch_size = 64, img_size = (
 
 
 
-def get_ckplus_data_gens(data_path = r'../data', batch_size = 64, img_size = (64, 64)):
+def get_non_split_data_gens(data_path = r'../data', batch_size = 64, img_size = (64, 64)):
     
 
     
@@ -102,7 +102,7 @@ def get_ckplus_data_gens(data_path = r'../data', batch_size = 64, img_size = (64
     
 def get_train_val_count(directory, dataset):
     
-    
+
     train_count = val_count = 0
     
     if dataset == 'FER_2013' or dataset == 'FER_2013+':
@@ -115,7 +115,7 @@ def get_train_val_count(directory, dataset):
                 val_count += 1   
                 
                 
-    elif dataset == 'CK+':
+    elif dataset == 'CK+' or dataset == 'JAFFE':
         
         for _, _, files in os.walk(directory):
             for Files in files:
@@ -128,3 +128,80 @@ def get_train_val_count(directory, dataset):
         
     return (train_count, val_count)
 
+
+
+def prep_jaffe(data_path):
+    
+    'Create output folder'
+    
+    new_path = data_path+'/prepped_jaffe'
+    
+    try:
+        os.makedirs(new_path)
+    except FileExistsError:
+        pass
+    
+    output_dirs = ['Disgust', 'Anger', 'Sad', 'Surprise', 'Neutral', 'Fear', 'Happy']
+    
+    
+    mapping = dict()
+    mapping['DI'] = 'Disgust'
+    mapping['AN'] = 'Anger'
+    mapping['SA'] = 'Sad'
+    mapping['SU'] = 'Surprise'
+    mapping['FE'] = 'Fear'
+    mapping['NE'] = 'Neutral'
+    mapping['HA'] = 'Happy'
+    
+    
+    dir_dict = dict()
+    for d in output_dirs:
+        new_dir = new_path + '/' + d
+        try:
+            os.makedirs(new_dir)
+        except FileExistsError:
+            pass
+        
+        dir_dict[d] = new_dir
+        
+
+
+    
+    for img in os.listdir(data_path):
+        splt = img.split('.')
+        if splt[-1] == 'tiff':
+            feeling = splt[1][0:2]
+            new_file_path = new_path + '/' + mapping[feeling] + '/' + img
+            print(new_file_path)
+            shutil.copyfile(data_path + '/' + img, new_file_path)
+            
+
+
+
+
+    
+    
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        
+        
+        
+        
